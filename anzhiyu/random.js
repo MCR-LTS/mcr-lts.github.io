b8f4f797bb4d52a8eaa86ee7c6fa1cc0fe1d23c8
MercuryLotus <mercurylotus@foxmail.com>
2026-03-12 12:49:56 +0800

    Site updated: 2026-03-12 12:49:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/08/woodpuaker/","2026/02/13/moectf2025/","2024/12/23/mimi/"];function toRandomPost(){
+var posts=["2026/02/13/moectf2025/","2024/12/23/mimi/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
