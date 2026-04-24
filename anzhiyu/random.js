7d30eb8e0add18dc27f7e562ada6a48004511cfa
MercuryLotus <mercurylotus@foxmail.com>
2026-04-24 09:17:30 +0800

    Site updated: 2026-04-24 09:17:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["dc-bot/","d-de-v-code/","eve-1/","mac-osu-otd/","mimi/","moectf2025-2/","humanmade-rev/","moectf2025/","unk-writeup-1/","tom-note/"];function toRandomPost(){
+var posts=["d-de-v-code/","dc-bot/","humanmade-rev/","eve-1/","mac-osu-otd/","mimi/","moectf2025-2/","tom-note/","moectf2025/","unk-writeup-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
