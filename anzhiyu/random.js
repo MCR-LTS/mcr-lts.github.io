2d67a280b5a388a05e622b5b1d51809d5e9b235d
MercuryLotus <mercurylotus@foxmail.com>
2026-04-30 01:52:27 +0800

    Site updated: 2026-04-30 01:52:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["d-de-v-code/","eve-1/","humanmade-rev/","mac-osu-otd/","mimi/","moectf2025-2/","tom-note/","unk-writeup-1/","moectf2025/","dc-bot/"];function toRandomPost(){
+var posts=["corn/","d-de-v-code/","dc-bot/","eve-1/","mimi/","mac-osu-otd/","protostar/","moectf2025-2/","timu/","tom-note/","unk-writeup-1/","moectf2025/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
