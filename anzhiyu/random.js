5bacdaa2a551b0866d0c50b84e342cdf4c5672c7
MercuryLotus <mercurylotus@foxmail.com>
2026-05-20 08:37:04 +0800

    私下学习社交技巧中……

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["corn/","d-de-v-code/","dc-bot/","eve-1/","mac-osu-otd/","mimi/","moectf2025/","syspmt/","timu/","moectf2025-2/","protostar/","unk-writeup-1/","tom-note/"];function toRandomPost(){
+var posts=["corn/","dc-bot/","eve-1/","mac-osu-otd/","mimi/","syspmt/","moectf2025-2/","protostar/","timu/","unk-writeup-1/","d-de-v-code/","moectf2025/","tom-note/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
